8e177fe565429c88464b9488ea9680d0a4a0fbab
TammyBusche <buschet@eou.edu>
2022-11-22 16:16:21 -0800

    Part 1: Polishing1 BufferTest.java

--- src/BufferTest.java
+++ src/BufferTest.java
@@ -9,7 +9,6 @@
 ◦Hint: BufferTest may need to keep track of how many threads have been sent the "terminate" message.
 ◦BufferTest should print out the thread name of any threads that have exited. */
 
-import java.util.InputMismatchException;
 import java.io.*;
 
 public class BufferTest implements Runnable{
@@ -27,12 +26,10 @@ public class BufferTest implements Runnable{
     public static void main (String[]args) {
         try {
             /* the first of which is the size of the BoundedBuffer */
-            int size = Integer.valueOf(args[0]);
+            int size = Integer.parseInt(args[0]);
 
             /* and the second of which is the number of Consumers to create. */
-            int numConsumers = Integer.valueOf(args[0]);
-
-            Console cmmdInput = System.console();
+            int numConsumers = Integer.parseInt(args[1]);
 
             /* ◦Create the BoundedBuffer, with the size being set from command line. */
             BoundedBuffer userBuffer = new BoundedBuffer(size);
@@ -42,7 +39,7 @@ public class BufferTest implements Runnable{
             Thread[] t = new Thread[numConsumers + 1]; // An array of threads for the consumers and main.
             Consumer[] userConsumers = new Consumer[numConsumers];          // An array of consumers.
             for (int i = 0; i < numConsumers; i++) {
-                String consumerName = "Consumer " + String.valueOf(i);
+                String consumerName = "Consumer " + i;
                 userConsumers[i] = new Consumer(userBuffer,consumerName);
                 t[i+1] = new Thread(userConsumers[i]);
             }
@@ -52,34 +49,31 @@ public class BufferTest implements Runnable{
 
             /* ◦Continually accept input strings from the user, creating a Message with the input string,
              and then calling the put() method on the BoundedBuffer, each time sending it the Message. */
+            Console cmmdInput = System.console();
             String inputStr = "";
-            /*while (inputStr != "terminate") {
+            while (!inputStr.equals("terminate")) {
                 System.out.println("Enter a string: ");
-                inputStr = cmmdInput.nextLine();
-            }*/
+                inputStr = cmmdInput.readLine();
+            }
             System.out.println("inputStr = " + inputStr);
-            //cmmdInput.close();
-
-        } catch (InputMismatchException e) {
+        } catch (Exception e) {
             /* ▪A "usage" statement should be printed if the parameters are not
              *  present, followed by a program exit. The "usage" statement should
              *  tell the user how to use the command line and give an example. */
-            System.out.println("---*** Parameter/s not received ***---");
-            System.out.println("A Bounded Buffer is used to communicate between threads.");
+            System.out.println("\t---**************** Error ****************---");
+            System.out.println("\t---******* Parameter/s not received ******---");
+            System.out.println("A Bounded Buffer will be used to communicate between threads.");
             System.out.println("This program will run a thread acting as a \"Producer\" which will \"write\" ");
-            System.out.println("into one end of the buffer. Meanwhile, another thread, the \"Consumer\",");
+            System.out.println("into one end of the buffer. Meanwhile, another thread/s, the \"Consumer/s\",");
             System.out.println("will \"read\" from the other as such:\n");
-            System.out.println("\t\t\t\t\t\t  Bounded Buffer");
-            System.out.println("\tProgram Writes to --> [][][][][][][] --> Comsumer/s Read from\n");
-            System.out.println("This example shows a Bounded Buffer with a length of \"7\". The user should ");
-            System.out.println("enter a numerical value/s corresponding to the requested input displayed ");
-            System.out.println("on the screen.");
-            System.out.println("Example:");
-            System.out.println("On-Screen - \"Please enter the size of the Bounded Buffer: 7\"");
-            System.out.println("User enters the number \"7\".");
-            System.out.println("On-Screen - \"Please enter the number of Consumers to create: 2\"");
-            System.out.println("User enters the number \"2\", meaning that two (2) Consumers will read ");
-            System.out.println("output from the Bounded Buffer.");
+            System.out.println("                       Bounded Buffer");
+            System.out.println("Producer Writes to --> [][][][][][][] --> Consumer/s Read from");
+            System.out.println("This example shows a Bounded Buffer with a length of \"7\".\n");
+            System.out.println("\t---*** Example Execution from cmd line ***---");
+            System.out.println("cmdline> java BufferTest 5 3");
+            System.out.println("5: The size of the buffer");
+            System.out.println("3: The number of consumers");
+            System.out.println("\t---***************************************---\n");
         }
     }
 }
